1e35918068e7ab435a6f6f8af0c17f0af1b42ac8
kkamenar <kkamenar@waukesha.k12.wius>
2022-07-13 14:25:42 -0500

    Through step 15.

--- Weather Swift UI/ContentView.swift
+++ Weather Swift UI/ContentView.swift
@@ -14,14 +14,29 @@ struct ContentView: View {
 //                .edgesIgnoringSafeArea(.all)
             LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
-        }
         
-        VStack{
-            Text("Chicago, IL")
-                .font(.system(size: 32, weight: .medium, design: .default))
-                .foregroundColor(.white)
-                .background(Color.red)
-                .frame(width: 200, height: 200, alignment: .center)
+            VStack{
+                Text("Chicago, IL")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    //.background(Color.red)
+                    //.frame(width: 200, height: 200, alignment: .center)
+                    .padding(.bottom, 100)
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 180)
+                    Text("76")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                    Spacer()
+              }
+        
+           
         }
     }
 }
