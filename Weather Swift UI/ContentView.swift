72c9a4a60620b6779ec4629527ea4421fa22db57
Meghan Lynn <mlynn@d127.org>
2022-07-13 14:14:37 -0500

    Added Linear Background and Chicago Text with Red Background

--- Weather Swift UI/ContentView.swift
+++ Weather Swift UI/ContentView.swift
@@ -10,8 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.blue
+//            Color.blue
+//                .edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+        }
+        
+        VStack{
+            Text("Chicago, IL")
+                .font(.system(size: 32, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .background(Color.red)
+                .frame(width: 200, height: 200, alignment: .center)
         }
     }
 }
